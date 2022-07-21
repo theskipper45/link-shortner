2e6f36b5fa371524075b696f11ac7d92cd4d87d8
Gaurav Diwakar <gdt5556@gmail.com>
2022-07-21 13:06:31 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import Advanced from "./components/Advanced"
+import Boost from "./components/Boost"
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import Shortener from "./components/Shortener"
+import Showcase from "./components/Showcase"
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Showcase />
+      <Shortener />
+      <Advanced />
+      <Boost />
+      <Footer />
+    </>
+  )
+}
